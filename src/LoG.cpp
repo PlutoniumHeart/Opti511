e83c161d478988f43368e47c74e71025954d0977
PlutoniumHeart <zhitaoli.hust@gmail.com>
2013-09-25 00:25:15 -0700

    Wrote real LoG edge filter.

--- src/LoG.cpp
+++ src/LoG.cpp
@@ -3,14 +3,105 @@
 
 int main(int argc, char** argv)
 {
-    LoGEdge* LoGFilter = new LoGEdge("../data/cmanswirl.pgm", 2.0);
+    if(argc<=10)
+    {
+        std::cerr<<"Usage: "<<argv[0]<<" -upper upperThreshold -lower lowerThreshold -sigma Sigma -i inputFile -o outputFile [-saveOriginalEdgeMap outputFile]"<<std::endl;
+		return -1;
+    }
+
+    int argCount = 1;
+    std::stringstream ss;
+    std::string inputFile;
+    std::string outputFile;
+    bool saveOriginalEdgeMap = false;
+    std::string originalEdgeMapOutput;
+    float UpperThreshold = 0.0, LowerThreshold = 0.0;
+    float sigma = 0.0;
+    bool inputFileSet = false;
+    bool outputFileSet = false;
+    bool upperThresholdSet = false;
+    bool lowerThresholdSet = false;
+    bool saveOriginal = false;
+
+    while(argCount<argc)
+    {
+        if(strcmp(argv[argCount], "-upper") == 0)
+        {
+            argCount++;
+            ss<<argv[argCount];
+            ss>>UpperThreshold;
+            ss.str(std::string());
+            ss.clear();
+            upperThresholdSet = true;
+            argCount++;
+        }
+        else if(strcmp(argv[argCount], "-lower") == 0)
+        {
+            argCount++;
+            ss<<argv[argCount];
+            ss>>LowerThreshold;
+            ss.str(std::string());
+            ss.clear();
+            lowerThresholdSet = true;
+            argCount++;
+        }
+        else if(strcmp(argv[argCount], "-i") == 0)
+        {
+            argCount++;
+            inputFile = argv[argCount];
+            inputFileSet = true;
+            argCount++;
+        }
+        else if(strcmp(argv[argCount], "-o") == 0)
+        {
+            argCount++;
+            outputFile = argv[argCount];
+            outputFileSet = true;
+            argCount++;
+        }
+        else if(strcmp(argv[argCount], "-sigma") == 0)
+        {
+            argCount++;
+            ss<<argv[argCount];
+            ss>>sigma;
+            ss.str(std::string());
+            ss.clear();
+            argCount++;
+        }
+        else if(strcmp(argv[argCount], "-saveOriginalEdgeMap") == 0)
+        {
+            argCount++;
+            originalEdgeMapOutput = argv[argCount];
+            saveOriginal = true;
+            argCount++;
+        }
+        else
+        {
+            std::cerr<<"Unknown command line argument!"<<std::endl;
+            return -1;
+        }
+    }
+
+    if(!(upperThresholdSet && lowerThresholdSet &&  inputFileSet &&outputFileSet))
+    {
+        std::cerr<<"Usage: "<<argv[0]<<" -upper upperThreshold -lower lowerThreshold -i inputFile -o outputFile [-saveOriginalEdgeMap outputFile]"<<std::endl;
+        return -1;
+    }
+
+    LoGEdge* LoGFilter = new LoGEdge(inputFile, sigma);
 
     LoGFilter->SetUpperThreshold(5);
     LoGFilter->SetLowerThreshold(3);
-    LoGFilter->SaveAsRaw(true);
+    if(saveOriginal)
+    {
+        LoGFilter->SaveAsRaw(true);
+    }
     LoGFilter->Filter();
-    LoGFilter->SaveResult("../data/LoGEdge_2.pgm");
-    LoGFilter->SaveRawResult("../data/LoGOriginal.pgm");
+    LoGFilter->SaveResult(outputFile);
+    if(saveOriginal)
+    {
+        LoGFilter->SaveRawResult(originalEdgeMapOutput);
+    }
 
     delete LoGFilter;
     return 0;
--- src/sobel.cpp
+++ src/sobel.cpp
@@ -94,4 +94,5 @@ int main(int argc, char** argv)
     }
 
 	delete SobelFilter;
+    return 0;
 }
