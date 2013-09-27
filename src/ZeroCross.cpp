38180fc4327cb7999e68cfa190604e734be1cb84
PlutoniumHeart <zhitaoli.hust@gmail.com>
2013-09-26 21:27:30 -0700

    Better ZC algorithm

--- src/ZeroCross.cpp
+++ src/ZeroCross.cpp
@@ -36,73 +36,71 @@ void ZeroCross::FindZeroCross(int col, int row, unsigned char** output)
         {
             std::vector<PixelLocation> Neighbors;
             output[i][j] = 255;
-            /*if( GetSign(m_ppSecondDerivative[i-1][j-1]) != GetSign(m_ppSecondDerivative[i+1][j+1]) )
+            if( GetSign(m_ppSecondDerivative[i-1][j-1]) != GetSign(m_ppSecondDerivative[i+1][j+1]) )
             {
-                output[i][j] += abs(m_ppSecondDerivative[i-1][j-1] - m_ppSecondDerivative[i+1][j+1])/(1.414*3.0);
+                if( GetSign(m_ppSecondDerivative[i][j]) == GetSign(m_ppSecondDerivative[i-1][j-1]) )
+                {
+                    if( abs(m_ppSecondDerivative[i][j]) <= abs(m_ppSecondDerivative[i+1][j+1]) )
+                    {
+                        output[i][j] = 0; // Black edge
+                    }
+                }
+                else
+                {
+                    if( abs(m_ppSecondDerivative[i][j]) <= abs(m_ppSecondDerivative[i-1][j-1]) )
+                    {
+                        output[i][j] = 0; // Black edge
+                    }
+                }
             }
             if( GetSign(m_ppSecondDerivative[i][j-1]) != GetSign(m_ppSecondDerivative[i][j+1]) )
             {
-                output[i][j] += abs(m_ppSecondDerivative[i][j-1] - m_ppSecondDerivative[i][j+1])/3.0;
+                if( GetSign(m_ppSecondDerivative[i][j]) == GetSign(m_ppSecondDerivative[i][j-1]) )
+                {
+                    if( abs(m_ppSecondDerivative[i][j]) <= abs(m_ppSecondDerivative[i][j+1]) )
+                    {
+                        output[i][j] = 0; // Black edge
+                    }
+                }
+                else
+                {
+                    if( abs(m_ppSecondDerivative[i][j]) <= abs(m_ppSecondDerivative[i][j+1]) )
+                    {
+                        output[i][j] = 0; // Black edge
+                    }
+                }
             }
             if( GetSign(m_ppSecondDerivative[i-1][j+1]) != GetSign(m_ppSecondDerivative[i+1][j-1]) )
             {
-                output[i][j] += abs(m_ppSecondDerivative[i-1][j+1] - m_ppSecondDerivative[i+1][j-1])/(1.414*3.0);
+                if( GetSign(m_ppSecondDerivative[i][j]) == GetSign(m_ppSecondDerivative[i-1][j+1]) )
+                {
+                    if( abs(m_ppSecondDerivative[i][j]) <= abs(m_ppSecondDerivative[i+1][j-1]) )
+                    {
+                        output[i][j] = 0; // Black edge
+                    }
+                }
+                else
+                {
+                    if( abs(m_ppSecondDerivative[i][j]) <= abs(m_ppSecondDerivative[i-1][j+1]) )
+                    {
+                        output[i][j] = 0; // Black edge
+                    }
+                }
             }
             if( GetSign(m_ppSecondDerivative[i-1][j]) != GetSign(m_ppSecondDerivative[i+1][j]) )
             {
-                output[i][j] += abs(m_ppSecondDerivative[i-1][j] - m_ppSecondDerivative[i+1][j])/3.0;
-            }*/
-            if( GetSign(m_ppSecondDerivative[i][j]) != GetSign(m_ppSecondDerivative[i-1][j]) )
-            {
-                Neighbors.push_back(PixelLocation(j, i-1));
-            }
-            if( GetSign(m_ppSecondDerivative[i][j]) != GetSign(m_ppSecondDerivative[i+1][j]) )
-            {
-                Neighbors.push_back(PixelLocation(j, i+1));
-            }
-            if( GetSign(m_ppSecondDerivative[i][j]) != GetSign(m_ppSecondDerivative[i][j-1]) )
-            {
-                Neighbors.push_back(PixelLocation(j-1, i));
-            }
-            if( GetSign(m_ppSecondDerivative[i][j]) != GetSign(m_ppSecondDerivative[i][j+1]) )
-            {
-                Neighbors.push_back(PixelLocation(j+1, i));
-            }
-
-            /*if( GetSign(m_ppSecondDerivative[i][j]) != GetSign(m_ppSecondDerivative[i-1][j-1]) )
-            {
-                Neighbors.push_back(PixelLocation(j-1, i-1));
-            }
-            if( GetSign(m_ppSecondDerivative[i][j]) != GetSign(m_ppSecondDerivative[i-1][j+1]) )
-            {
-                Neighbors.push_back(PixelLocation(j+1, i-1));
-            }
-            if( GetSign(m_ppSecondDerivative[i][j]) != GetSign(m_ppSecondDerivative[i+1][j-1]) )
-            {
-                Neighbors.push_back(PixelLocation(j-1, i+1));
-            }
-            if( GetSign(m_ppSecondDerivative[i][j]) != GetSign(m_ppSecondDerivative[i+1][j+1]) )
-            {
-                Neighbors.push_back(PixelLocation(j+1, i+1));
-            }*/
-
-            if(Neighbors.empty())
-            {
-                output[i][j] = 255;
-            }
-            else
-            {
-                std::vector<PixelLocation>::iterator itr;
-                for(itr=Neighbors.begin();itr!=Neighbors.end();itr++)
+                if( GetSign(m_ppSecondDerivative[i][j]) == GetSign(m_ppSecondDerivative[i-1][j]) )
                 {
-                    if(abs(m_ppSecondDerivative[i][j])>abs(m_ppSecondDerivative[itr->row][itr->col]))
+                    if( abs(m_ppSecondDerivative[i][j]) <= abs(m_ppSecondDerivative[i+1][j]) )
                     {
-                        output[i][j] = 255;
-                        break;
+                        output[i][j] = 0; // Black edge
                     }
-                    else
+                }
+                else
+                {
+                    if( abs(m_ppSecondDerivative[i][j]) <= abs(m_ppSecondDerivative[i-1][j]) )
                     {
-                        output[i][j] = 0;
+                        output[i][j] = 0; // Black edge
                     }
                 }
             }
