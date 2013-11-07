c055a84a6a568d2a26a0a181c489cee6c339ffa1
PlutoniumHeart <zhitaoli.hust@gmail.com>
2013-11-06 23:49:12 -0700

    Prototypes for next assignment

---
+++ src/Dilation.cpp
@@ -0,0 +1,8 @@
+#include "BaseImage.h"
+#include "ArgumentParsing.h"
+
+
+int main(int argc, char** argv)
+{
+  return 0;
+}
---
+++ src/Erosion.cpp
@@ -0,0 +1,7 @@
+#include "BaseImage.h"
+#include "ArgumentParsing.h"
+
+int main(int argc, char** argv)
+{
+  return 0;
+}
